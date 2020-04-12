bfff12c3369c8a1c6c01a766cd70d790fbba3e1d
philhuan <hjw@hjwblog.com>
2020-04-13 00:07:16 +0800

    init

---
+++ src/main/java/com/hjwblog/robo_cmp/RoboCmpApplication.java
@@ -0,0 +1,13 @@
+package com.hjwblog.robo_cmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboCmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboCmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hjwblog/robo_cmp/RoboCmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjwblog.robo_cmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboCmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
